53cf05f7d8dd4b2a0213bf6b907d7254ebb5bc09
kt19027 <112942217+kt19027@users.noreply.github.com>
2022-09-06 18:20:56 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,37 @@
+let degrees = 0
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45 || degrees > 315) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (degrees < 135) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (degrees < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
